73d49519348fe73742d34c9240ecd760f44b58a5
Bohdan-Dubravin <dubravin111@gmail.com>
2023-03-29 07:45:34 +0300

    edit user profile route

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+  minLength,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isAdmin?: boolean;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { User, UserDocument } from './models/user.model';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ForbiddenException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
@@ -11,5 +13,14 @@ export class UserService {
     const user = await this.userModel.findById(userId);
 
     return user;
+  }
+
+  async updateProfile(userId: string, dto: UpdateUserDto) {
+    const user = await this.getUserProfile(userId);
+    const isSameUser = await this.userModel.findOne({ email: dto.email });
+
+    if (isSameUser && String(user._id) !== String(isSameUser._id)) {
+      throw new ForbiddenException('Email already taken');
+    }
   }
 }
