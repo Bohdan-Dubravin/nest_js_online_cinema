d68ebc3550af2f11b93849fc09cc669ba48c71c7
Bohdan-Dubravin <dubravin111@gmail.com>
2023-04-22 22:11:24 +0300

    add cors headers

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,32 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://online-cinema-nextjs-pa9fl9bo5-bohdan-dubravin.vercel.app',
-      'http://www.example.com',
-      'http://app.example.com',
-      'https://example.com',
-      'https://www.example.com',
-      'https://app.example.com',
-    ],
-    methods: ['GET', 'POST'],
-    credentials: true,
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://online-cinema-nextjs-pa9fl9bo5-bohdan-dubravin.vercel.app',
+  //     'http://www.example.com',
+  //     'http://app.example.com',
+  //     'https://example.com',
+  //     'https://www.example.com',
+  //     'https://app.example.com',
+  //   ],
+  //   methods: ['GET', 'POST'],
+  //   credentials: true,
+  // });
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 4200);
 }
