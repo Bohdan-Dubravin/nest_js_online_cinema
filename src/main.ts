10cf644a7e069bc8b5c179efd4079ea09f7361e0
Bohdan-Dubravin <dubravin111@gmail.com>
2023-04-23 11:52:27 +0300

    allow origin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.enableCors({
   //   origin: [
   //     'http://localhost:3000',
@@ -17,12 +18,13 @@ async function bootstrap() {
   //   methods: ['GET', 'POST'],
   //   credentials: true,
   // });
-
+  app.use();
   app.enableCors({
-    allowedHeaders: '*',
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     origin: '*',
     credentials: true,
-    // Access-Control-Allow-Origin: https://online-cinema-nextjs-pa9fl9bo5-bohdan-dubravin.vercel.app
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
